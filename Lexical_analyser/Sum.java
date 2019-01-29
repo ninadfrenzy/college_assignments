95b136048877fe625ddde4f1d5776e320a79b4ac
Ninad manjaramkar <32587826+ninadfrenzy@users.noreply.github.com>
2019-01-29 10:17:24 +0530

    Add files via upload

---
+++ Lexical_analyser/Sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Sum
+{
+	public static void main(String args[])
+	{
+		int a,b,result;
+		a = 5;
+		b = 7;
+		result = a+b;
+		System.out.println("sum of these numbers"+result); 
+	}
+
+}
